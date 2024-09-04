ef5db4ce6d333551ae99bca5a2512b07dc7a44da
kitparl <pranshu.java@gmail.com>
2024-09-04 22:35:28 +0530

    updated neo

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("run_program_popup.run_program")
+
+require("nvim-devdocs").setup({
+  additional_docs = { "java" },
+})
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,5 +3,21 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("n", "<leader>rn", ":IncRename ")
+vim.api.nvim_set_keymap("n", "<leader>dd", "diwi", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>dc", "diwi", { noremap = true, silent = true })
 
-vim.api.nvim_set_keymap("n", "<leader>rj", ":w<CR>:term java %<CR>", { noremap = true, silent = true })
+vim.api.nvim_create_user_command("OpenConfig", function()
+  -- Open nvim-tree
+  vim.cmd("NvimTreeToggle")
+
+  -- Open the init.lua file in the main window
+  vim.cmd("edit ~/.config/nvim/lua/lazyvim/config/init.lua")
+end, {})
+
+vim.api.nvim_create_user_command("OpenPlugin", function()
+  -- Open nvim-tree
+  vim.cmd("NvimTreeToggle")
+
+  -- Optionally navigate to a specific directory or file
+  -- vim.cmd("edit ~/.config/nvim/plugins/") -- Uncomment if needed
+end, {})
